af0c4201e823b8ce83aa04ce6955dd9f71caff62
MiletaBorisova <miletaborisova5@gmail.com>
2025-10-07 11:15:33 +0300

    calculator

---
+++ Calculator2025/Calculator2025/Calculator/Calculator/OptionsManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public static class OptionsManager
+    {
+        public static string[] OptionsList = {
+            "ex - Exit",
+            "a - Add",
+            "s - Subtract",
+            "m - Multiply",
+             };
+
+        public static string Add(double a, double b)
+        {
+            return $"{a} + {b} = {a + b}";
+        }
+
+        public static string Multiply(double a, double b)
+        {
+            return $"{a} x {b} = {a * b}";
+        }
+
+        public static string Subtract(double a, double b)
+        {
+            return $"{a} - {b} = {a - b}";
+        }
+
+    }
+}
